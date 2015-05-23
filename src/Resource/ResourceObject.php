bcaa5a2d8a2b2f0505e210a38a7992568fe1b809
Matthias Bayer <thematthiasbayer@gmail.com>
2015-05-23 22:37:00 +0200

    Improve attribute and relationship handling

--- src/Resource/ResourceObject.php
+++ src/Resource/ResourceObject.php
@@ -101,7 +101,51 @@ class ResourceObject
      */
     public function setAttributes($attributes)
     {
-        $this->attributes = $attributes;
+        $this->attributes = null;
+
+        foreach ($attributes as $name => $value) {
+            $this->setAttribute($name, $value);
+        }
+    }
+
+    /**
+     * @param $name
+     * @return string|array|null
+     */
+    public function getAttribute($name)
+    {
+        if (isset($this->attributes[$name])) {
+            return $this->attributes[$name];
+        }
+
+        return null;
+    }
+
+    /**
+     * @param string $name
+     * @param string|array $value
+     */
+    public function setAttribute($name, $value)
+    {
+        if (is_array($value)) {
+            /**
+             * @link http://jsonapi.org/format/#document-structure-resource-attributes
+             * Complex data structures MUST reserve the relationships and links members for future use.
+             */
+            if (!empty($value['relationships']) || !empty($value['links'])) {
+                throw new \InvalidArgumentException("Complex data structures must not contain 'relationships' and 'links' members.");
+            }
+        }
+
+        if (isset($this->relationships[$name])) {
+            throw new \InvalidArgumentException("Attribute name '$name' is invalid as a relationship with the same name already exists'");
+        }
+
+        if (null === $this->attributes) {
+            $this->attributes = array();
+        }
+
+        $this->attributes[$name] = $value;
     }
 
     /**
@@ -125,8 +169,20 @@ class ResourceObject
         return null;
     }
 
-    public function setRelationship($name, $value)
+    /**
+     * @param string $name
+     * @param Relationship $value
+     */
+    public function setRelationship($name, Relationship $value)
     {
+        if (isset($this->attributes[$name])) {
+            throw new \InvalidArgumentException("Relationship name '$name' is invalid as an attribute with the same name already exists'");
+        }
+
+        if (null === $this->relationships) {
+            $this->relationships = array();
+        }
+
         $this->relationships[$name] = $value;
     }
 
@@ -135,7 +191,7 @@ class ResourceObject
      */
     public function setRelationships(array $relationships)
     {
-        $this->relationships = array();
+        $this->relationships = null;
 
         foreach ($relationships as $name => $value) {
             $this->setRelationship($name, $value);
